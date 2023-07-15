5951f543375521eb064d503463e9fbe7ce726540
Colin Casey <FLCInnovationCenterTV@gmail.com>
2023-07-14 19:37:05 -0700

    renamed vars and removed cluttered

--- scripts/pushimagesnew.py
+++ scripts/pushimagesnew.py
@@ -1,13 +1,13 @@
 import subprocess
 
-pi_host = "10.54.0.210"
-pi_username = "picam"
-pi_password = "tract0rSpy2.1"
+bee_host = "10.54.0.210"
+bee_username = "picam"
+bee_password = "tract0rSpy2.1"
 
-remote_file = r"C:\Users\flcin\Documents"
-local_file = "/home/pi/Desktop/timelapsestorage" # whole folder
-image = "/home/pi/Desktop/timelapsestorage/0firstImage.jpg" # one image at a time
+destination = r"C:\Users\flcin\Documents"
+folder_file = "/home/pi/Desktop/timelapsestorage" # whole folder
+image_file = "/home/pi/Desktop/timelapsestorage/0firstImage.jpg" # one image at a time
 
-newcommand = ["sshpass", "-p", pi_password, "scp", "-r", local_file, f"{pi_username}@{pi_host}:{remote_file}"]
+newcommand = ["sshpass", "-p", bee_password, "scp", "-r", image_file, f"{bee_username}@{bee_host}:{destination}"]
 
-completed_process = subprocess.run(newcommand, input=pi_password, text=True, capture_output=True)
+completed_process = subprocess.run(newcommand, text=True, capture_output=True)
