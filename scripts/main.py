08c595c8e121c0c9f61ebc6a042f0bd8585b06bd
FLC-Innovation-Center-TV <134670609+FLC-Innovation-Center-TV@users.noreply.github.com>
2023-08-21 15:37:31 -0700

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -86,7 +86,9 @@ print("output configured")
     #    time.sleep(interval)
 
 if __name__ == "__main__":
+    print("mainfunction entered")
     picam2.start_recording(encoder, output)
+    print("picam2 started recording
     time.sleep(10)
     #take_timelapse(interval=15, duration=60*30, imagestoragepath=imagestoragepath)
     time.sleep(9999999)
